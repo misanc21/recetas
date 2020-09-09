6961847fa774e8c534bda06235f285a76f5267ef
misanc21 <misanc21@gmail.com>
2020-09-08 20:05:15 -0500

    comenzando

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <header className="bg-alert">
+            Busca recetas de bedidas
+        </header>
+    );
+}
+ 
+
+export default Header;
