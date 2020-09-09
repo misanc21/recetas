386d88571c2656932ab74a9cc72f9607a72bdeef
misanc21 <misanc21@gmail.com>
2020-09-09 01:40:08 -0500

    add eslint

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -34,7 +34,7 @@ const RecetasProvider = (props) => {
             }
             obtenerRecetas();
         }
-
+    // eslint-disable-next-line no-use-before-define
     }, [busque])
 
     return(
